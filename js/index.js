08a8a5968ec6d23b5b1c77ae7a30648a98d11a01
Ilya Kurakov <makemedoit@mail.ru>
2022-03-29 10:58:31 +0300

    Первый комит

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("Привет!")
+
+
+const string = 'hello'
+
+console.log("typeof number: ", typeof number)
+console.log("typeof string: ", typeof string)
+number = "sdfdasfdsfdsfds"
+console.log(number)
+
+const number = 0
+
